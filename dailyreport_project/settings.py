c9fd8bd668181b5b5e66faf55fed4eb27834ea67
SHADIK <shadik.sk420@gmail.com>
2025-07-21 14:19:43 +0600

    Updates time zone and adds login redirect URLs
    Changes the time zone from UTC to Asia/Dhaka to align with the application's target region.
    Adds default redirect URLs for login and logout to improve user navigation.

--- dailyreport_project/settings.py
+++ dailyreport_project/settings.py
@@ -108,11 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
 USE_TZ = True
+
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
