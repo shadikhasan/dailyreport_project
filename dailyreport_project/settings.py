e2766d1f05af97afc62938def226c22bfd064995
SHADIK <shadik.sk420@gmail.com>
2025-07-22 15:12:34 +0600

    Switches to environment-based configuration and PostgreSQL
    Replaces hardcoded settings with environment variables for better
    security and flexibility. Updates database configuration to use
    PostgreSQL instead of SQLite. Adds `.env` management to `.gitignore`
    and `.dockerignore`. Includes `dotenv` in requirements for managing
    environment variables.

--- dailyreport_project/settings.py
+++ dailyreport_project/settings.py
@@ -10,22 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--5d)ya9+y^m%&a-7#(hd%*xn2-z_1_qi8z&8u*8#j_x1ti+%sg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://dailyreport-project.onrender.com',
@@ -82,8 +79,12 @@ WSGI_APPLICATION = 'dailyreport_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
